a11a355938f47122208a20e86b2d018819d7bce4
Dustin Spicuzza <dustin@virtualroadside.com>
2018-02-08 00:52:26 -0500

    Initial commit

---
+++ robot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import magicbot
+import wpilib
+
+
+class Robot(magicbot.MagicRobot):
+    
+    def createObjects(self):
+        pass
+    
+    def teleopInit(self):
+        pass
+        
+    def teleopPeriodic(self):
+        pass
+
+
+if __name__ == '__main__':
+    wpilib.run(Robot)
