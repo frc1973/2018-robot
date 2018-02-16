6648cd315d41ef8cfde0e1b33b0c6c7efc789847
Vilarj <cjimenez18@bostonk12.org>
2018-02-15 19:27:26 -0500

    Mid Elevator works

---
+++ components/elevator.py
@@ -0,0 +1,41 @@
+
+import wpilib
+
+class Elevator:
+
+    elevator_motor = wpilib.Talon
+
+    elevator_top = wpilib.DigitalInput
+    elevator_mid = wpilib.DigitalInput
+    elevator_bottom = wpilib.DigitalInput
+    active = False
+
+    speed = 0
+
+    def up(self):
+        self.speed = -1
+
+    def down(self):
+        self.speed = 1
+
+    def gotoTop(self):
+        pass
+
+    def gotoMiddle(self):
+        self.active = True
+
+    def gotoBottom(self):
+        pass
+
+    def execute(self): # This is the start
+        """
+        self.speed = 0"""
+
+        if self.active:
+            self.speed = -1
+            if self.elevator_mid.get():
+                self.speed = 0
+                self.active = False
+
+        self.elevator_motor.set(self.speed)
+        speed = 0
--- robot.py
+++ robot.py
@@ -3,17 +3,35 @@
 import magicbot
 import wpilib
 
+import components.elevator
+
 
 class Robot(magicbot.MagicRobot):
-    
+
+    elevator = components.elevator.Elevator
+
     def createObjects(self):
-        pass
-    
+
+        # Elevator motor/sensors
+        self.elevator_motor = wpilib.Talon(1)
+        self.elevator_top = wpilib.DigitalInput(1)
+        self.elevator_mid = wpilib.DigitalInput(2)
+        self.elevator_bottom = wpilib.DigitalInput(3)
+
+        self.joy = wpilib.Joystick(0)
+
     def teleopInit(self):
         pass
-        
+
     def teleopPeriodic(self):
-        pass
+        if self.joy.getRawButton(1):
+            self.elevator.up()
+        if self.joy.getRawButton(2):
+            self.elevator.down()
+        if self.joy.getRawButton(4):
+            self.elevator.gotoTop()
+        if self.joy.getRawButton(3):
+            self.elevator.gotoMiddle()
 
 
 if __name__ == '__main__':
