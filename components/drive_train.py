1fe8cf2410883c7909996f9fb5b6d58e1146b8be
hornom <lehornom@gmail.com>
2018-02-19 15:35:19 -0500

    Made the robot drive

--- components/drive_train.py
+++ components/drive_train.py
@@ -1,8 +1,6 @@
 import math
 import wpilib
 
-from magicbot #import tunable
-
 class DriveTrain:
     '''
         Simple magicbot drive object
@@ -18,8 +16,8 @@ class DriveTrain:
 
     #MaxY = tunable(0.8)
 
-    ultrasonic = wpilib.AnalogInput
-    myRobot = wpilib.RobotDrive
+    #ultrasonic = wpilib.AnalogInput
+    drive = wpilib.drive.DifferentialDrive
 
     def __init__(self):
         self.x = 0
@@ -32,41 +30,10 @@ class DriveTrain:
     def rotate(self, x):
         self.x = x
 
-    #def atWall(self):
-    #    return abs(self.getDistance()) < 0.15
-
-    #def driveToWall(self):
-
-    #    distance = self.getDistance()
-    #    distance = max(min(14.0, distance), -self.offset)
-
-    #    if abs(distance)<0.15:
-    #        self.y = 0
-    #        self.x = 0
-    #    else:
-
-    #        y = self.wall_p*distance
-
-#            scaled_y = 0.5 + abs(y)/30.0
-#            scaled_y = math.copysign(scaled_y, y)
-
-#            scaled_y = max(min(self.MaxY, scaled_y), -self.MaxY)
-
-#            self.y = scaled_y
-
-
-
-#    def getDistance(self):
-        # returns distance in feet
-#        return (self.ultrasonic.getAverageVoltage()/0.3) - self.offset
-
     def execute(self):
         self.tx = self.x
         self.ty = self.y
-        self.myRobot.arcadeDrive(self.y, self.x, True)
-
-        self.distance = self.getDistance()
-        self.analog = self.ultrasonic.getAverageVoltage()
+        self.drive.arcadeDrive(self.y, self.x, True)
 
         self.x = 0
         self.y = 0
--- robot.py
+++ robot.py
@@ -2,23 +2,33 @@
 
 import magicbot
 import wpilib
+import wpilib.drive
 
 import components.elevator
+import components.drive_train
 
 
 class Robot(magicbot.MagicRobot):
 
     elevator = components.elevator.Elevator
+    driveTrain = components.drive_train.DriveTrain
 
     def createObjects(self):
 
         # GOTTA DO STUFF
 
         # Elevator motor/sensors
-        self.elevator_motor = wpilib.Talon(1)
+        self.elevator_motor = wpilib.Talon(2)
         self.elevator_top = wpilib.DigitalInput(1)
         self.elevator_mid = wpilib.DigitalInput(2)
         self.elevator_bottom = wpilib.DigitalInput(3)
+
+        #Drive motors
+        self.left_motor = wpilib.Spark(0)
+        self.right_motor = wpilib.Spark(1)
+        self.drive = wpilib.drive.DifferentialDrive(self.left_motor, self.right_motor)
+
+
 
         self.joy = wpilib.Joystick(0)
 
@@ -26,6 +36,7 @@ class Robot(magicbot.MagicRobot):
         pass
 
     def teleopPeriodic(self):
+        self.driveTrain.move(self.joy.getY(), self.joy.getX())
         if self.joy.getRawButton(1):
             self.elevator.up()
         if self.joy.getRawButton(2):
