6c42bad16c27a76aec93f178025e917f57af256a
Vilarj <cjimenez18@bostonk12.org>
2018-03-01 20:12:07 -0500

    Forklift
    * Creation of a forklift
    - There are a bunch of functions related to the joystick; winch, position, speed, region

---
+++ bare/Forklift.py
@@ -0,0 +1,96 @@
+import wpilib
+import wpilib.drive
+
+import ctre
+
+
+class Forklift():
+    GOING_UP = 1
+    GOING_DOWN = -1
+    SLOW = 1.0/3.0
+    FAST = 0.9
+
+    STATE_TOP = 0
+    STATE_NEAR_TOP = 1
+    STATE_MIDDLE = 2
+    STATE_NEAR_BOTTOM = 3
+    STATE_BOTTOM = 4
+
+
+    def __init__(self, joystick, winch_motor, potentiometer):
+        self.joystick = joystick
+        self.winch_motor = winch_motor
+        self.potentiometer = potentiometer
+
+    def getWinchSpeedLimit(self, state, direction):
+            """
+
+            Get the Max speed of the winch motor
+            based on where the Forklift is
+
+            """
+
+        if state == STATE_TOP and direction == GOING_UP:
+            return 0
+        elif state == STATE_NEAR_TOP and direction == GOING_UP:
+            return SLOW
+        elif state == STATE_BOTTOM and direction == GOING_DOWN:
+            return 0
+        elif state == STATE_NEAR_BOTTOM and direction == GOING_DOWN:
+            return SLOW
+        else:
+            return FAST
+
+    def positionToState(self):
+        """
+
+            Convert the potentiometer position
+            to state
+
+        """
+        # TODO: fix these numbers once we calibrate the potentiometer
+        POSITION_TOP = 10000
+        POSITION_MID_TOP = 8000
+        POSITION_MID_BOTTOM = 2000
+        POSITION_BOTTOM = 1000
+
+        position = self.potentiometer.getPosition() # TODO figure out how to get potentiometer value
+
+        if position >= POSITION_TOP:
+            return STATE_TOP # Stop
+        elif position < POSITION_TOP and position >= POSITION_MID_TOP:
+            return STATE_NEAR_TOP # Keep going
+        elif position < POSITION _MID_TOP and position >= POSITION_MID_BOTTOM:
+            return STATE_NEAR_MIDDLE # Keep going
+        elif position < POSITION_MID_BOTTOM and position >= POSITION_BOTTOM:
+            return STATE_NEAR_BOTTOM # Stop
+        else:
+            return NEAR_BOTTOM
+
+    def joystickCommandToDirection(self):
+        """
+
+            Convert forklift winch motor commanded
+            value to a direction
+        """
+        joystick = self.joystick.getY() # TODO: verify Y is forward/back
+        if joystick_command > 0:
+            return GOING_UP
+        elif joystick_command < 0:
+            return GOING_DOWN
+        else:
+            return 0
+
+    def setSpeed(self):
+        state = self.positionToState()
+        direction = self.joystickCommandToDirection()
+        max_speed = self.getWinchSpeedLimit(state, direction)
+
+        #TODO check joystick speed commands (buttons) and set speed based on Joystick
+        # and speed limit
+        # Obey the joystick, unless it goes above the speed limit
+
+        if speed > max_speed:
+            # TODO: set winch motor speed
+            # self.winch_motor.
+            pass
--- bare/robot.py
+++ bare/robot.py
@@ -8,6 +8,7 @@ import wpilib.drive
 
 import ctre
 
+from Forklift import Forklift
 
 class MyRobot(wpilib.IterativeRobot):
 
@@ -18,20 +19,29 @@ class MyRobot(wpilib.IterativeRobot):
         """
         self.left_motor = ctre.WPI_TalonSRX(5)
         self.right_motor = ctre.WPI_TalonSRX(6)
+
         self.drive = wpilib.drive.DifferentialDrive(self.left_motor, self.right_motor)
 
-        self.stick = wpilib.Joystick(0)
-        self.stick2 = wpilib.Joystick(1)
+        self.drive_stick = wpilib.Joystick(0)
+        self.forklift_stick = wpilib.Joystick(1)
 
-        self.other_motor1 = ctre.WPI_TalonSRX(7)
+        self.winch_motor = ctre.WPI_TalonSRX(7)
         self.other_motor2 = ctre.WPI_TalonSRX(8)
+
+        self.potentiometer = None # TODO: add this
+        self.forklift = Forklift(self.forklift_stick, self.winch_motor, self.potentiometer)
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
-        self.drive.arcadeDrive(self.stick.getY(), self.stick.getX())
+        self.drive.arcadeDrive(self.drive_stick.getY(), self.drive_stick.getX())
 
-        self.other_motor1.set(self.stick.getZ())
-        self.other_motor2.set(self.stick2.getZ())
+        self.winch_motor.set(self.forklift_stick.getZ())
+        self.other_motor2.set(self.forklift_stick.getZ())
 
+        self.forklift_stick.getTrigger()
+    # --------------------------------------------------------------------------
+
+
+    # --------------------------------------------------------------------------
 if __name__ == "__main__":
     wpilib.run(MyRobot)
